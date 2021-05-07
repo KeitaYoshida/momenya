dc56f650b0aa1dafb0dc0e0b2c89a93882adcb0d
KeitaYoshida <ba0503naky@gmail.com>
2021-05-07 14:46:14 +0900

    add message

--- src/components/home-top/top-slider.tsx
+++ src/components/home-top/top-slider.tsx
@@ -14,12 +14,12 @@ const TopSlider: React.FC = () => {
       <OpacityBox time={1}>
         <div className="py-5 px-3 py-sm-5 px-sm-5 my-8 top-message">
           <div className="overflow-hidden">
-            <h1 className="fs-1 fs-md-2">Top Message</h1>
-            {/* <h1 className="fs-1 fs-md-2">木綿屋モーターサービス</h1> */}
+            <h1 className="fs-1 fs-md-2">車のサポート　お任せください</h1>
           </div>
           <div className="overflow-hidden">
             <p className="color-primary mt-2 mb-2 lh-2 fs-0 fs-md-1 ">
-              紹介文言。text text text text text text text text
+              木綿屋モーターサービスでは、施設内にガソリンスタンドと車の修理工場を併設し、ガソリンスタンドだけでも、修理工場だけでも行き届かない、様々なサービスを提供しております。
+              また、新車・中古車販売、カー保険、ロードサービスも行っており、車に関する様々なお悩み、ご相談にも対応できます。まずはお気軽にお問い合わせください。
             </p>
           </div>
           <div className="overflow-hidden">
