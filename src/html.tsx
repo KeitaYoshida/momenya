77da2f104f249fb904eb2f36f1952d2ac8a68a6f
KeitaYoshida <ba0503naky@gmail.com>
2025-10-20 16:05:27 +0900

    up

--- src/html.tsx
+++ src/html.tsx
@@ -14,6 +14,17 @@ const HTML: React.FC<Props> = ({
   return (
     <html lang="jp">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJ4T3VQR');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -26,6 +37,16 @@ const HTML: React.FC<Props> = ({
         <link href="/img/favicon.png" rel="icon" type="image/x-icon" />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJ4T3VQR"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <div id="___gatsby" dangerouslySetInnerHTML={{ __html: body }} />
         {postBodyComponents}
         <script
